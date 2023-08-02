4dc17982bac52a09d2f0e3db8a85e1d693f22c45
Evgenii Ivanov <98652070+EugeneMyCode@users.noreply.github.com>
2023-08-02 23:24:21 +0700

    Update base_page.py
    Del excess imports

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,8 +2,6 @@
 
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 
 
 class BasePage:
